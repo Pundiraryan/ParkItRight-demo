8c2391c32153119174330568ee32d3e6adf11aaa
Lakshay Kansal <lakshaykansal04@gmail.com>
2023-09-19 16:39:39 +0530

    check

--- api/routes/booking.js
+++ api/routes/booking.js
@@ -5,7 +5,7 @@ const {
   createBookings,
   getBookings,
 } = require('../controllers/bookingController');
-
+// console.log("hello world"); 
 router.route('/').get(getBookings).post(createBookings);
 
 module.exports = router;
