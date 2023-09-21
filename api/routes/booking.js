9965489287b6cf4d92b1387eadd183807f743f60
Aryan <aryanpundir0606@gmail.com>
2023-09-21 20:38:30 +0530

    booking changes

--- api/routes/booking.js
+++ api/routes/booking.js
@@ -5,21 +5,11 @@ const {
   createBookings,
   getBookings,
 } = require('../controllers/bookingController');
-<<<<<<< HEAD
-//<<<<<<< HEAD
-
-// Protected routes (user must be logged in)
-router.route('/').get(isLoggedIn, getBookings).post(isLoggedIn, createBookings);
-//=======
-// console.log("hello world"); 
 router.route('/').get(getBookings).post(createBookings);
-//>>>>>>> 8c2391c32153119174330568ee32d3e6adf11aaa
-=======
-
+// router.route('/').get(getBookings).post(createBookings);
 // Protected routes (user must be logged in)
-router.route('/').get(isLoggedIn, getBookings).post(isLoggedIn, createBookings);
+// router.route('/').get(isLoggedIn, getBookings).post(isLoggedIn, createBookings);
 // console.log("hello world"); 
-router.route('/').get(getBookings).post(createBookings);
->>>>>>> 63d6f834f7939164896a6c3b160a8df43763cf71
+// router.route('/').get(getBookings).post(createBookings);
 
 module.exports = router;
--- api/routes/place.js
+++ api/routes/place.js
@@ -10,13 +10,14 @@ const {
   userPlaces,
   searchPlaces
 } = require('../controllers/placeController');
-
 router.route('/').get(getPlaces);
-
 // Protected routes (user must be logged in)
-router.route('/add-places').post(isLoggedIn, addPlace);
-router.route('/user-places').get(isLoggedIn, userPlaces);
-router.route('/update-place').put(isLoggedIn, updatePlace);
+// router.route('/add-places').post(isLoggedIn, addPlace);
+router.route('/add-places').post(addPlace);
+// router.route('/user-places').get(isLoggedIn, userPlaces);
+router.route('/user-places').get(userPlaces);
+// router.route('/update-place').put(isLoggedIn, updatePlace);
+router.route('/update-place').put(updatePlace);
 // Not Protected routed but sequence should not be interfered with above routes
 router.route('/:id').get(singlePlace);
 router.route('/search/:key').get(searchPlaces)
